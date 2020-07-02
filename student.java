a0bc8fe3a6f6521a80f829e27dbe09a6191fb519
STRATTON <strattonmmei8@gmail.com>
2020-07-02 19:28:43 +0300

    new changes

--- student.java
+++ student.java
@@ -5,9 +5,13 @@ class Student{
 		int age = 20;
 		String name = "John Doe";
 		String studentClass = "Form1";
+		String address = "Nairobi";
+		String admissionNumber = "dhd221";
 
 		System.out.println("Name: " +name);
 		System.out.println("Age: " +age);
 		System.out.println("Class: " +studentClass);
+		System.out.println("Address: " +address);
+		System.out.println("AdmissionNumber: " +admissionNumber);
 	}
 }
